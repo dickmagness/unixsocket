a44416e03fb43c4484e5b37753776bbe5e8badde
dickmagness <magness.richard@gmail.com>
2023-09-24 13:25:00 -0500

    updates

--- entrypoint-custom.sh
+++ entrypoint-custom.sh
@@ -1,7 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Start the sql proxy
-cloud_sql_proxy -instances=$CLOUDSQL_INSTANCE=tcp:3306 &
+# Function to determine the web server type
+web_server_type() {
+  # Logic to determine the web server type
+  # You can use environment variables or any other method here
+  echo "apache"  # Example: Set to "apache"
+}
 
-# Execute the rest of your ENTRYPOINT and CMD as expected.
-exec "$@"
+# Determine the web server type
+WEB_SERVER=$(web_server_type)
+
+# Execute the appropriate script based on the web server type
+if [ "$WEB_SERVER" = "apache" ]; then
+  exec /opt/bitnami/scripts/moodle/run.sh
+elif [ "$WEB_SERVER" = "nginx" ]; then
+  exec /opt/bitnami/scripts/nginx-php-fpm/run.sh
+else
+  echo "Unsupported web server type: $WEB_SERVER"
+  exit 1
+fi
